41196ced2e17fab55c120c06d47c37a3c354ecee
Pizza0409 <luoyunsheng@YS-LO.local>
2024-11-28 15:50:20 +0800

    Finish a, b, c, d, e.

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -26,14 +26,17 @@ def robert(img_arr, threshold):
     res_arr = np.zeros([img_size0, img_size1])
     for i in range(img_size0-1):
         for j in range(img_size1-1):  
+            '''
+            r1 = [-1 0
+                   0 1]
+            r2 = [[0 -1
+                   1  0]
+            '''
             r1 =  - int(img_arr[i][j]) + int(img_arr[i+1][j+1])
             r2 = - int(img_arr[i][j+1]) + int(img_arr[i+1][j])
+            
             grad = np.sqrt(r1**2 + r2**2)
-
-            if grad < threshold:
-                res_arr[i, j] = 255
-            else:
-                res_arr[i, j]= 0
+            res_arr[i, j] = 255 if grad < threshold else 0
     
     return res_arr
 
@@ -42,7 +45,7 @@ img_robert = im.fromarray(img_arr_robert.astype(np.uint8))
 # img_robert.save("./img_robert.bmp")
 # img_robert.show()
 
-def prewitts_edge(img_arr, threshold):
+def prewitt_edge(img_arr, threshold):
     img_arr = expand_with_replicate(img_arr, 1)
     m, n = img_arr.shape
     res_arr = np.zeros([img_size0, img_size1])
@@ -61,19 +64,16 @@ def prewitts_edge(img_arr, threshold):
             p2 = - int(img_arr[i, j]) - int(img_arr[i+1, j]) - int(img_arr[i+2, j]) + int(img_arr[i, j+2]) + int(img_arr[i+1, j+2]) + int(img_arr[i+2, j+2])
 
             grad = np.sqrt(p1**2 + p2**2)
-            if grad < threshold:
-                res_arr[i, j] = 255
-            else:
-                res_arr[i, j] = 0
+            res_arr[i, j] = 255 if grad < threshold else 0
     
     return res_arr
 
-img_arr_prewitts = prewitts_edge(lena_arr, 24)
-img_prewitts = im.fromarray(img_arr_prewitts.astype(np.uint8))
-# img_prewitts.save("./img_prewitts.bmp")
+img_arr_prewitt = prewitt_edge(lena_arr, 24)
+img_prewitt = im.fromarray(img_arr_prewitt.astype(np.uint8))
+# img_prewitt.save("./img_prewitt.bmp")
 # img_prewitts.show()
 
-def sobels_edge(img_arr, threshold):
+def sobel_edge(img_arr, threshold):
     img_arr = expand_with_replicate(img_arr, 1)
     m, n = img_arr.shape
     res_arr = np.zeros([img_size0, img_size1])
@@ -92,19 +92,16 @@ def sobels_edge(img_arr, threshold):
             s2 = - int(img_arr[i, j]) - 2 * int(img_arr[i+1, j]) - int(img_arr[i+2, j]) + int(img_arr[i, j+2]) + 2 * int(img_arr[i+1, j+2]) + int(img_arr[i+2, j+2])
 
             grad = np.sqrt(s1**2 + s2**2)
-            if grad < threshold:
-                res_arr[i, j] = 255
-            else:
-                res_arr[i, j] = 0
+            res_arr[i, j] = 255 if grad < threshold else 0
     
     return res_arr    
 
-img_arr_sobels = sobels_edge(lena_arr, 38)
-img_sobels = im.fromarray(img_arr_sobels.astype(np.uint8))
-# img_sobels.save("./img_sobels.bmp")
+img_arr_sobel = sobel_edge(lena_arr, 38)
+img_sobel = im.fromarray(img_arr_sobel.astype(np.uint8))
+# img_sobel.save("./img_sobel.bmp")
 # img_sobels.show()
 
-def frel_and_chens_grad(img_arr, threshold):
+def frel_and_chen_grad(img_arr, threshold):
     img_arr = expand_with_replicate(img_arr, 1)
     m, n = img_arr.shape
     res_arr = np.zeros([img_size0, img_size1])
@@ -123,14 +120,81 @@ def frel_and_chens_grad(img_arr, threshold):
             s2 = - int(img_arr[i, j]) - np.sqrt(2) * int(img_arr[i+1, j]) - int(img_arr[i+2, j]) + int(img_arr[i, j+2]) + np.sqrt(2) * int(img_arr[i+1, j+2]) + int(img_arr[i+2, j+2])
 
             grad = np.sqrt(s1**2 + s2**2)
-            if grad < threshold:
-                res_arr[i, j] = 255
-            else:
-                res_arr[i, j] = 0
+            res_arr[i, j] = 255 if grad < threshold else 0
     
     return res_arr
 
-img_arr_frel_a_chens = frel_and_chens_grad(lena_arr, 30)
-img_frel_a_chens = im.fromarray(img_arr_frel_a_chens.astype(np.uint8))
-img_frel_a_chens.save("./img_frel_a_chens.bmp")
-img_frel_a_chens.show()
+img_arr_frel_a_chen = frel_and_chen_grad(lena_arr, 30)
+img_frel_a_chen = im.fromarray(img_arr_frel_a_chen.astype(np.uint8))
+# img_frel_a_chen.save("./img_frel_a_chen.bmp")
+# img_frel_a_chens.show()
+
+def kirsch_comass(img_arr, threshold):
+    img_arr = expand_with_replicate(img_arr, 1)
+    m, n = img_arr.shape
+    res_arr = np.zeros([img_size0, img_size1])
+
+    for i in range(img_size0):
+        for j in range(img_size1):
+            '''
+            k0 = [-3    -3     5
+                  -3     0     5
+                  -3    -3     5]
+            k1 = [-3     5     5
+                  -3     0     5
+                  -3    -3    -3]
+            k2 = [5     5     5
+                 -3     0    -3
+                 -3    -3    -3]
+            k3 = [5    5     -3
+                  5    0     -3
+                 -3   -3     -3]
+            k4 = [5    -3     -3
+                  5     0     -3
+                  5    -3     -3]
+            k5 = [-3    -3     -3
+                   5     0     -3
+                   5     5     -3]
+            k6 = [-3    -3     -3
+                  -3     0     -3
+                   5     5     5]
+            k7 = [-3    -3    -3
+                  -3     0     5
+                  -3     5     5]
+            '''
+            k0 = - 3 * int(img_arr[i, j]) - 3 * int(img_arr[i, j+1]) + 5 * int(img_arr[i, j+2]) \
+                - 3 * int(img_arr[i+1, j]) + 5 * int(img_arr[i+1, j+2]) \
+                - 3 * int(img_arr[i+2, j]) - 3 * int(img_arr[i+2, j+1]) + 5 * int(img_arr[i+2, j+2])
+            k1 = - 3 * int(img_arr[i, j]) + 5 * int(img_arr[i, j+1]) + 5 * int(img_arr[i, j+2]) \
+                - 3 * int(img_arr[i+1, j]) + 5 * int(img_arr[i+1, j+2]) \
+                - 3 * int(img_arr[i+2, j]) - 3 * int(img_arr[i+2, j+1]) - 3 * int(img_arr[i+2, j+2])
+            k2 = 5 * int(img_arr[i, j]) + 5 * int(img_arr[i, j+1]) + 5 * int(img_arr[i, j+2]) \
+                - 3 * int(img_arr[i+1, j]) - 3 * int(img_arr[i+1, j+2]) \
+                - 3 * int(img_arr[i+2, j]) - 3 * int(img_arr[i+2, j+1]) - 3 * int(img_arr[i+2, j+2])
+            k3 = 5 * int(img_arr[i, j]) + 5 * int(img_arr[i, j+1]) - 3 * int(img_arr[i, j+2]) \
+                + 5 * int(img_arr[i+1, j]) - 3 * int(img_arr[i+1, j+2]) \
+                - 3 * int(img_arr[i+2, j]) - 3 * int(img_arr[i+2, j+1]) - 3 * int(img_arr[i+2, j+2])
+            k4 = 5 * int(img_arr[i, j]) - 3 * int(img_arr[i, j+1]) - 3 * int(img_arr[i, j+2]) \
+                + 5 * int(img_arr[i+1, j]) - 3 * int(img_arr[i+1, j+2]) \
+                + 5 * int(img_arr[i+2, j]) - 3 * int(img_arr[i+2, j+1]) - 3 * int(img_arr[i+2, j+2])
+            k5 = - 3 * int(img_arr[i, j]) - 3 * int(img_arr[i, j+1]) - 3 * int(img_arr[i, j+2]) \
+                + 5 * int(img_arr[i+1, j]) - 3 * int(img_arr[i+1, j+2]) \
+                + 5 * int(img_arr[i+2, j]) + 5 * int(img_arr[i+2, j+1]) - 3 * int(img_arr[i+2, j+2])
+            k6 = - 3 * int(img_arr[i, j]) - 3 * int(img_arr[i, j+1]) - 3 * int(img_arr[i, j+2]) \
+                - 3 * int(img_arr[i+1, j]) - 3 * int(img_arr[i+1, j+2]) \
+                + 5 * int(img_arr[i+2, j]) + 5 * int(img_arr[i+2, j+1]) + 5 * int(img_arr[i+2, j+2])
+            k7 = - 3 * int(img_arr[i, j]) - 3 * int(img_arr[i, j+1]) - 3 * int(img_arr[i, j+2]) \
+                - 3 * int(img_arr[i+1, j]) + 5 * int(img_arr[i+1, j+2]) \
+                - 3 * int(img_arr[i+2, j]) + 5 * int(img_arr[i+2, j+1]) + 5 * int(img_arr[i+2, j+2])
+
+            k_list = np.array([k0, k1, k2, k3, k4, k5, k6, k7])
+            grad = np.max(k_list)
+
+            res_arr[i, j] = 255 if grad < threshold else 0
+    
+    return res_arr
+
+img_arr_kirsch_compass = kirsch_comass(lena_arr, 135)
+img_kirsch_compass = im.fromarray(img_arr_kirsch_compass.astype(np.uint8))
+# img_kirsch_compass.save("./img_kirsch_compass.bmp")
+# img_kirsch_compass.show()
