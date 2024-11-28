6a1018bc340eea21d7443c083842cd3678d0923c
Pizza0409 <ee27187219@gmail.com>
2024-11-28 23:51:57 +0800

    Finish HW9 exclude report.

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -71,7 +71,7 @@ def prewitt_edge(img_arr, threshold):
 img_arr_prewitt = prewitt_edge(lena_arr, 24)
 img_prewitt = im.fromarray(img_arr_prewitt.astype(np.uint8))
 # img_prewitt.save("./img_prewitt.bmp")
-# img_prewitts.show()
+# img_prewitt.show()
 
 def sobel_edge(img_arr, threshold):
     img_arr = expand_with_replicate(img_arr, 1)
@@ -99,7 +99,7 @@ def sobel_edge(img_arr, threshold):
 img_arr_sobel = sobel_edge(lena_arr, 38)
 img_sobel = im.fromarray(img_arr_sobel.astype(np.uint8))
 # img_sobel.save("./img_sobel.bmp")
-# img_sobels.show()
+# img_sobel.show()
 
 def frel_and_chen_grad(img_arr, threshold):
     img_arr = expand_with_replicate(img_arr, 1)
@@ -127,7 +127,7 @@ def frel_and_chen_grad(img_arr, threshold):
 img_arr_frel_a_chen = frel_and_chen_grad(lena_arr, 30)
 img_frel_a_chen = im.fromarray(img_arr_frel_a_chen.astype(np.uint8))
 # img_frel_a_chen.save("./img_frel_a_chen.bmp")
-# img_frel_a_chens.show()
+# img_frel_a_chen.show()
 
 def kirsch_comass(img_arr, threshold):
     img_arr = expand_with_replicate(img_arr, 1)
@@ -137,30 +137,30 @@ def kirsch_comass(img_arr, threshold):
     for i in range(img_size0):
         for j in range(img_size1):
             '''
-            k0 = [-3    -3     5
-                  -3     0     5
-                  -3    -3     5]
-            k1 = [-3     5     5
-                  -3     0     5
-                  -3    -3    -3]
-            k2 = [5     5     5
-                 -3     0    -3
-                 -3    -3    -3]
-            k3 = [5    5     -3
-                  5    0     -3
-                 -3   -3     -3]
-            k4 = [5    -3     -3
-                  5     0     -3
-                  5    -3     -3]
-            k5 = [-3    -3     -3
-                   5     0     -3
-                   5     5     -3]
-            k6 = [-3    -3     -3
-                  -3     0     -3
-                   5     5     5]
-            k7 = [-3    -3    -3
-                  -3     0     5
-                  -3     5     5]
+            k0 = [-3   -3    5
+                  -3    0    5
+                  -3   -3    5]
+            k1 = [-3    5    5
+                  -3    0    5
+                  -3   -3   -3]
+            k2 = [5     5    5
+                 -3     0   -3
+                 -3    -3   -3]
+            k3 = [5     5   -3
+                  5     0   -3
+                 -3    -3   -3]
+            k4 = [5    -3   -3
+                  5     0   -3
+                  5    -3   -3]
+            k5 = [-3   -3   -3
+                   5    0   -3
+                   5    5   -3]
+            k6 = [-3   -3   -3
+                  -3    0   -3
+                   5    5    5]
+            k7 = [-3   -3   -3
+                  -3    0    5
+                  -3    5    5]
             '''
             k0 = - 3 * int(img_arr[i, j]) - 3 * int(img_arr[i, j+1]) + 5 * int(img_arr[i, j+2]) \
                 - 3 * int(img_arr[i+1, j]) + 5 * int(img_arr[i+1, j+2]) \
@@ -198,3 +198,123 @@ img_arr_kirsch_compass = kirsch_comass(lena_arr, 135)
 img_kirsch_compass = im.fromarray(img_arr_kirsch_compass.astype(np.uint8))
 # img_kirsch_compass.save("./img_kirsch_compass.bmp")
 # img_kirsch_compass.show()
+
+def robinson_compass(img_arr, threshold):
+    img_arr = expand_with_replicate(img_arr, 1)
+    m, n = img_arr.shape
+    res_arr = np.zeros([img_size0, img_size1])
+
+    for i in range(img_size0):
+        for j in range(img_size1):
+            '''
+            r0 = [-1    0     1
+                  -2    0     2
+                  -1    0     1]
+            r1 = [0     1     2
+                 -1     0     1
+                 -2    -1     0]
+            r2 = [1     2     1
+                  0     0     0
+                 -1    -2    -1]
+            r3 = [2     1     0
+                  1     0    -1
+                  0    -1    -2]
+            r4 = [1     0    -1
+                  2     0    -2
+                  1     0    -1]
+            r5 = [0    -1    -2
+                  1     0    -1
+                  2     1     0]
+            r6 = [-1   -2    -1
+                   0    0     0
+                   1    2     1]
+            r7 = [-2   -1     0
+                  -1    0     1
+                   0    1     2]
+            '''
+            r0 = - int(img_arr[i, j]) + int(img_arr[i, j+2]) \
+                - 2 * int(img_arr[i+1, j]) + 2 * int(img_arr[i+1, j+2]) \
+                - int(img_arr[i+2, j]) + int(img_arr[i+2, j+2])
+            r1 = int(img_arr[i, j+1]) + 2 * int(img_arr[i, j+2]) \
+                - int(img_arr[i+1, j]) + int(img_arr[i+1, j+2]) \
+                - 2 * int(img_arr[i+2, j]) - int(img_arr[i+2, j+1])
+            r2 = int(img_arr[i, j]) + 2 * int(img_arr[i, j+1]) + int(img_arr[i, j+2]) \
+                - int(img_arr[i+2, j]) - 2 * int(img_arr[i+2, j+1]) - int(img_arr[i+2, j+2])
+            r3 = 2 * int(img_arr[i, j]) + int(img_arr[i, j+1]) \
+                + int(img_arr[i+1, j]) - int(img_arr[i+1, j+2]) \
+                - int(img_arr[i+2, j+1]) - 2 * int(img_arr[i+2, j+2])
+            r4 = int(img_arr[i, j]) - int(img_arr[i, j+2]) \
+                + 2 * int(img_arr[i+1, j]) - 2 * int(img_arr[i+1, j+2]) \
+                + int(img_arr[i+2, j]) - int(img_arr[i+2, j+2])
+            r5 = - int(img_arr[i, j+1]) - 2 * int(img_arr[i, j+2]) \
+                + int(img_arr[i+1, j]) - int(img_arr[i+1, j+2]) \
+                + 2 * int(img_arr[i+2, j]) + int(img_arr[i+2, j+1])
+            r6 = - int(img_arr[i, j]) - 2 * int(img_arr[i, j+1]) - int(img_arr[i, j+2]) \
+                + int(img_arr[i+2, j]) + 2 * int(img_arr[i+2, j+1]) + int(img_arr[i+2, j+2])
+            r7 = - 2 * int(img_arr[i, j]) - int(img_arr[i, j+1]) \
+                - int(img_arr[i+1, j]) + int(img_arr[i+1, j+2]) \
+                + int(img_arr[i+2, j+1]) + 2 * int(img_arr[i+2, j+2])
+
+            r_list = np.array([r0, r1, r2, r3, r4, r5, r6, r7])
+            grad = np.max(r_list)
+
+            res_arr[i, j] = 255 if grad < threshold else 0
+    
+    return res_arr
+
+img_arr_robinson = robinson_compass(lena_arr, 43)
+img_robinson = im.fromarray(img_arr_robinson.astype(np.uint8))
+# img_robinson.save("./img_robinson.bmp")
+# img_robinson.show()
+
+def nevatia_babu_5x5(img_arr, threshold):
+# 定義 Nevatia-Babu 算子 
+    kernels = [ 
+        np.array([[ 100, 100, 100, 100, 100], 
+                [ 100, 100, 100, 100, 100], 
+                [ 0, 0, 0, 0, 0], 
+                [-100, -100, -100, -100, -100], 
+                [-100, -100, -100, -100, -100]]), 
+        np.array([[ 100, 100, 100, 100, 100], 
+                [ 100, 100, 100, 78, -32], 
+                [ 100, 92, 0, -92, -100], 
+                [ 32, -78, -100, -100, -100], 
+                [-100, -100, -100, -100, -100]]),
+        np.array([[ 100, 100, 100, 32, -100], 
+                [ 100, 100, 92, -78, -100], 
+                [ 100, 100, 0, -100, -100], 
+                [ 100, 78, -92, -100, -100], 
+                [ 100, -32, -100, -100, -100]]), 
+        np.array([[-100, -100, 0, 100, 100], 
+                [-100, -100, 0, 100, 100], 
+                [-100, -100, 0, 100, 100], 
+                [-100, -100, 0, 100, 100], 
+                [-100, -100, 0, 100, 100]]),
+        np.array([[-100, 32, 100, 100, 100], 
+                [-100, -78, 92, 100, 100], 
+                [-100, -100, 0, 100, 100], 
+                [-100, -100, -92, 78, 100], 
+                [-100, -100, -100, -32, 100]]), 
+        np.array([[ 100, 100, 100, 100, 100], 
+                [ -32, 78, 100, 100, 100], 
+                [-100, -92, 0, 92, 100], 
+                [-100, -100, -100, -78, 32], 
+                [-100, -100, -100, -100, -100]]) 
+    ] 
+    img_arr = expand_with_replicate(img_arr, 2) 
+    res_arr = np.zeros([img_size0, img_size1]) 
+    for i in range(img_size0): 
+        for j in range(img_size1): 
+            grads = [] 
+            for kernel in kernels: 
+                region = img_arr[i:i+5, j:j+5] 
+                grad = np.sum(kernel * region) 
+                grads.append(grad) 
+            max_grad = np.max(grads) 
+            res_arr[i, j] = 0 if max_grad > threshold else 255 
+    return res_arr
+
+img_arr_nevatia_babu = nevatia_babu_5x5(lena_arr, 12500)
+img_nevatia_babu = im.fromarray(img_arr_nevatia_babu.astype(np.uint8))
+img_nevatia_babu.save("./img_nevatia_babu.bmp")
+img_nevatia_babu.show()
